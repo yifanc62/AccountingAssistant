e2ffe71c5837218eff98678679dcc0706f90f18c
Saika <381207510@qq.com>
2017-07-13 17:08:18 +0800

    流水详情和新建流水页面

---
+++ app/src/main/java/com/cirnoteam/accountingassistant/NewRecord.java
@@ -0,0 +1,127 @@
+package com.cirnoteam.accountingassistant;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.cirnoteam.accountingassistant.R.id.amount_edit;
+
+/**
+ * Created by Saika on 2017/7/13.
+ */
+
+public class NewRecord extends AppCompatActivity{
+    //临时变量
+    String inout,type,account;
+    float amount=0;
+
+    private List<String> list_inout = new ArrayList<String>();
+    private List<String> list_type = new ArrayList<String>();
+    private List<String> list_account = new ArrayList<String>();
+    private Spinner spinner_inout;
+    private Spinner spinner_type;
+    private Spinner spinner_account;
+    private ArrayAdapter<String> adapter_inout;
+    private ArrayAdapter<String> adapter_type;
+    private ArrayAdapter<String> adapter_account;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_record);
+
+        list_inout.add("支出");
+        list_inout.add("收入");
+        list_type.add("一日三餐");
+        list_type.add("购物消费");
+        list_type.add("水电煤气");
+        list_type.add("交通花费");
+        list_type.add("医疗消费");
+        list_type.add("其他支出");
+        list_type.add("经营获利");
+        list_type.add("工资收入");
+        list_type.add("路上捡钱");
+        list_type.add("其他收入");
+        while (true) {
+            //TODO 查询数据库中的账户表，添加到list_account
+            break;
+        }
+
+
+        spinner_inout = (Spinner) findViewById(R.id.spinner_inout);
+        spinner_type = (Spinner) findViewById(R.id.spinner_type);
+        spinner_account = (Spinner) findViewById(R.id.spinner_account);
+        adapter_inout = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list_inout);
+        adapter_type = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list_type);
+        adapter_account = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list_account);
+        spinner_inout.setAdapter(adapter_inout);
+        spinner_type.setAdapter(adapter_type);
+        spinner_account.setAdapter(adapter_account);
+        EditText editText = (EditText) findViewById(amount_edit);
+
+        /*******************监听事件***********************/
+        spinner_inout.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+
+                inout = adapter_inout.getItem(arg2).toString();
+                //Toast.makeText(getApplicationContext(), adapter_inout.getItem(arg2), Toast.LENGTH_SHORT).show();
+                arg0.setVisibility(View.VISIBLE);
+            }
+
+            public void onNothingSelected(AdapterView<?> arg0) {
+                //Toast.makeText(getApplicationContext(), "none", Toast.LENGTH_SHORT).show();
+                arg0.setVisibility(View.VISIBLE);
+            }
+        });
+        spinner_type.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+
+                type = adapter_type.getItem(arg2).toString();
+                //Toast.makeText(getApplicationContext(), adapter_type.getItem(arg2), Toast.LENGTH_SHORT).show();
+                arg0.setVisibility(View.VISIBLE);
+            }
+
+            public void onNothingSelected(AdapterView<?> arg0) {
+                Toast.makeText(getApplicationContext(), "none", Toast.LENGTH_SHORT).show();
+                arg0.setVisibility(View.VISIBLE);
+            }
+        });
+        spinner_account.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+
+                account = adapter_account.getItem(arg2).toString();
+                //Toast.makeText(getApplicationContext(), adapter_account.getItem(arg2), Toast.LENGTH_SHORT).show();
+                arg0.setVisibility(View.VISIBLE);
+            }
+
+            public void onNothingSelected(AdapterView<?> arg0) {
+
+                Toast.makeText(getApplicationContext(), "none", Toast.LENGTH_SHORT).show();
+                arg0.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+    public void toCreate(View view) {
+        EditText editText = (EditText) findViewById(amount_edit);
+        if(!TextUtils.isEmpty(editText.getText()))
+            amount = Float.parseFloat(editText.getText().toString());
+        else
+            amount = 0;
+        Toast.makeText(getApplicationContext(), amount+inout+account+type, Toast.LENGTH_SHORT).show();
+        //TODO 存储新流水 inout收支(String) account账户(String) type类型(String) amount金额(float)
+        //返回代码还没写
+    }
+    public void back(View view){
+        //返回代码还没写
+    }
+}
